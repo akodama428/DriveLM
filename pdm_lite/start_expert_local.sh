979065ca7863d5db953450eee08aeab97d545a10
atsushi <kodamaatsushi428@gmail.com>
2024-12-15 21:59:00 +0900

    PDM_LITEによる学習データ生成

--- pdm_lite/start_expert_local.sh
+++ pdm_lite/start_expert_local.sh
@@ -17,12 +17,21 @@ term() {
 }
 trap term SIGINT
 
+export CARLA_ROOT="/home/atsushi/DriveLM/pdm_lite/carla/CARLA_Leaderboard_20"
+export WORK_DIR="/home/atsushi/DriveLM/pdm_lite"
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
+export SCENARIO_RUNNER_ROOT=${WORK_DIR}/scenario_runner
+export LEADERBOARD_ROOT=${WORK_DIR}/leaderboard
+export PYTHONPATH="${CARLA_ROOT}/PythonAPI/carla/":"${SCENARIO_RUNNER_ROOT}":"${LEADERBOARD_ROOT}":${PYTHONPATH}
+
 # carla
 export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
 export REPETITIONS=1
-export DEBUG_CHALLENGE=0
-
-export PTH_ROUTE=${WORK_DIR}/leaderboard/data/routes_devtest
+export DEBUG_CHALLENGE=1
+# export PTH_ROUTE=${WORK_DIR}/leaderboard/data/routes_devtest
+export PTH_ROUTE=/home/atsushi/carla_garage/Bench2Drive/leaderboard/data/bench2drive220_0_pdm_lite_traj
+export IS_BENCH2DRIVE=1
 
 # Function to handle errors
 handle_error() {
@@ -47,14 +56,14 @@ export TM_PORT=$((PORT + 3))
 
 export CHECKPOINT_ENDPOINT=${PTH_ROUTE}.json
 export TEAM_CONFIG=${PTH_ROUTE}.xml
-export PTH_LOG='logs'
+export PTH_LOG='log_root/logs_2'
 export RESUME=1
-export DATAGEN=0
-export SAVE_PATH='logs'
+export DATAGEN=1
+export SAVE_PATH='log_root/logs_2'
 export TM_SEED=0
 
 # Start the actual evaluation / data generation
-python leaderboard/leaderboard/leaderboard_evaluator_local.py --port=${PORT} --traffic-manager-port=${TM_PORT} --routes=${ROUTES} --repetitions=${REPETITIONS} --track=${CHALLENGE_TRACK_CODENAME} --checkpoint=${CHECKPOINT_ENDPOINT} --agent=${TEAM_AGENT} --agent-config=${TEAM_CONFIG} --debug=0 --resume=${RESUME} --timeout=2000 --traffic-manager-seed=${TM_SEED}
+CUDA_VISIBLE_DEVICES=0 python leaderboard/leaderboard/leaderboard_evaluator_local.py --port=${PORT} --traffic-manager-port=${TM_PORT} --routes=${ROUTES} --repetitions=${REPETITIONS} --track=${CHALLENGE_TRACK_CODENAME} --checkpoint=${CHECKPOINT_ENDPOINT} --agent=${TEAM_AGENT} --agent-config=${TEAM_CONFIG} --debug=0 --resume=${RESUME} --timeout=2000 --traffic-manager-seed=${TM_SEED}
 
 # Kill the Carla server afterwards
 pkill Carla
